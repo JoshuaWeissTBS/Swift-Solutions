91c215132d934bb8f420e45032c7738cd3ca4e80
Joshua Weiss <joshua.weiss@hotmail.com>
2024-04-06 19:30:09 -0700

    ran model scaffold, project does not build yet

--- source/PopNGo/PopNGo/PopNGo/Models/PopNGoDB.cs
+++ source/PopNGo/PopNGo/PopNGo/Models/PopNGoDB.cs
@@ -15,6 +15,8 @@ public partial class PopNGoDB : DbContext
     {
     }
 
+    public virtual DbSet<BookmarkList> BookmarkLists { get; set; }
+
     public virtual DbSet<Event> Events { get; set; }
 
     public virtual DbSet<EventHistory> EventHistories { get; set; }
@@ -25,19 +27,28 @@ public partial class PopNGoDB : DbContext
 
     public virtual DbSet<Tag> Tags { get; set; }
 
-/*    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Name=ServerConnection");*/
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ServerConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<BookmarkList>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Bookmark__3214EC2702C96ED5");
+
+            entity.HasOne(d => d.User).WithMany(p => p.BookmarkLists)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BookmarkList_UserID");
+        });
+
         modelBuilder.Entity<Event>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Event__3214EC27F708465D");
+            entity.HasKey(e => e.Id).HasName("PK__Event__3214EC2747CFE6CD");
         });
 
         modelBuilder.Entity<EventHistory>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__EventHis__3214EC27C75E2682");
+            entity.HasKey(e => e.Id).HasName("PK__EventHis__3214EC2722D88687");
 
             entity.HasOne(d => d.Event).WithMany(p => p.EventHistories)
                 .OnDelete(DeleteBehavior.ClientSetNull)
@@ -50,25 +61,25 @@ public partial class PopNGoDB : DbContext
 
         modelBuilder.Entity<FavoriteEvent>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Favorite__3214EC2746FCA72D");
+            entity.HasKey(e => e.Id).HasName("PK__Favorite__3214EC2711C85E0B");
+
+            entity.HasOne(d => d.BookmarkList).WithMany(p => p.FavoriteEvents)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_FavoriteEvents_BookmarkListID");
 
             entity.HasOne(d => d.Event).WithMany(p => p.FavoriteEvents)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_FavoriteEvents_EventID");
-
-            entity.HasOne(d => d.User).WithMany(p => p.FavoriteEvents)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_FavoriteEvents_UserID");
         });
 
         modelBuilder.Entity<PgUser>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__PG_User__3214EC278B002AAE");
+            entity.HasKey(e => e.Id).HasName("PK__PG_User__3214EC272EA60077");
         });
 
         modelBuilder.Entity<Tag>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Tag__3214EC275300977C");
+            entity.HasKey(e => e.Id).HasName("PK__Tag__3214EC27EBF99BE6");
         });
 
         OnModelCreatingPartial(modelBuilder);
