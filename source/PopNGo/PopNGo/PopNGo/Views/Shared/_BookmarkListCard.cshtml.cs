99df6bca6aa87420accb2ef19a488e34cd0754a4
Joshua Weiss <joshua.weiss@hotmail.com>
2024-04-07 19:18:20 -0700

    created BookmarkListCard partial

---
+++ source/PopNGo/PopNGo/PopNGo/Views/Shared/_BookmarkListCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PopNGo.Views.Shared
+{
+    public class _BookmarkListCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
